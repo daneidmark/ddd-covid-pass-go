71687c6bf1a2bf989eb2a3ce4871d5685e65beef
Dan Eidmark <dan.eidmark@gmail.com>
2021-11-10 12:53:55 +0100

    Initial commit

---
+++ aggregate.go
@@ -0,0 +1,42 @@
+package main
+
+import "time"
+
+type Version uint64
+type AggregateId string
+type AggregateRoot struct {
+	Id                AggregateId
+	Version           Version
+	Timestamp         time.Time
+	UncommittedEvents []Event
+}
+
+type Event struct {
+	AggregateId AggregateId
+	Version     Version
+	Timestamp   time.Time
+	Data        interface{}
+}
+
+type Aggregate interface {
+	Transition(e Event)
+}
+
+func (ar *AggregateRoot) Apply(a Aggregate, e Event) {
+	a.Transition(e)
+	ar.Id = e.AggregateId
+	ar.Version = e.Version
+	ar.Timestamp = e.Timestamp
+}
+
+func (ar *AggregateRoot) ApplyNew(a Aggregate, Data interface{}) {
+	e := Event{AggregateId: ar.Id, Version: ar.Version + 1, Timestamp: time.Now(), Data: Data}
+	ar.UncommittedEvents = append(ar.UncommittedEvents, e)
+	ar.Apply(a, e)
+}
+
+func (ar *AggregateRoot) BuildFromHistory(a Aggregate, events []Event) {
+	for _, event := range events {
+		ar.Apply(a, event)
+	}
+}
---
+++ aggregate_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Implement test Aggregate
+type Name string
+
+type Person struct {
+	AggregateRoot
+	Name Name
+}
+
+func (p *Person) birth(n Name) {
+	p.ApplyNew(p, &Born{Name: n})
+}
+
+func (person *Person) Transition(e Event) {
+	switch e := e.Data.(type) {
+	case *Born:
+		person.Name = e.Name
+	}
+}
+
+type Born struct {
+	Name Name
+}
+
+// tests
+
+func TestPersonBorn(t *testing.T) {
+	person := Person{}
+	person.birth("Pelle")
+	if person.Name != "Pelle" {
+		t.Fatal("The name is not correct")
+	}
+
+	if len(person.UncommittedEvents) != 1 {
+		t.Fatal("No events has been registered")
+	}
+
+	if reflect.TypeOf(person.UncommittedEvents[0].Data).String() != "*main.Born" {
+		t.Fatal("Born is not the first event " + reflect.TypeOf(person.UncommittedEvents[0].Data).String())
+	}
+}
